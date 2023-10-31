ae2f58d6dde8728fc69b53ec49c54d294a8124f2
Harshvardhan Sharma <hvsharma63@gmail.com>
2023-11-01 01:20:17 +0530

    cleanup

--- src/IoiCityMallSalesFileServiceProvider.php
+++ src/IoiCityMallSalesFileServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace RetailCosmos\IoiCityMallSalesFile;
 
-use RetailCosmos\IoiCityMallSalesFile\Commands\IoiCityMallSalesFileCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -10,16 +9,8 @@ class IoiCityMallSalesFileServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('ioi-city-mall-sales-file')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_ioi-city-mall-sales-file_table')
-            ->hasCommand(IoiCityMallSalesFileCommand::class);
+            ->hasConfigFile();
     }
 }
