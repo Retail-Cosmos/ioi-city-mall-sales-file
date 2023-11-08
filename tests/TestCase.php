9305ec8b2b1537fc9b6af3e6172e6de193744091
Harshvardhan Sharma <hvsharma63@gmail.com>
2023-11-08 22:12:27 +0530

    Test Case Updated

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -21,8 +21,6 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('ioi-city-mall-sales-file.disk_to_use', 'local');
-        config()->set('ioi-city-mall-sales-file.first_file_generation_date', '2023-10-22');
         config()->set('database.default', 'testing');
     }
 }
