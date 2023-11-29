06e5dfb8ad6fd165686afff952be8e5897e61246
Harshvardhan Sharma <hvsharma63@gmail.com>
2023-11-29 13:50:32 +0530

    update: PaymentType Enum

--- src/Enums/PaymentType.php
+++ src/Enums/PaymentType.php
@@ -18,18 +18,8 @@ enum PaymentType: string
 
     case OTHERS = 'others';
 
-    public static function names(): array
-    {
-        return array_column(self::cases(), 'name');
-    }
-
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
-    }
-
-    public static function array(): array
-    {
-        return array_combine(self::values(), self::names());
     }
 }
